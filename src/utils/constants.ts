0aa26dac63f0da18586ffc16724447a20cf848a5
Sabin Adams <sabintheworld@gmail.com>
2022-08-29 14:50:32 -0700

    Moves function to Prisma org

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const EXAMPLES_REPO_URL =
   "https://api.github.com/repos/prisma/prisma-examples/git/trees/latest?recursive=1";
 
-export const EXAMPLES_REPO_TAR = 'https://create-prisma-interceptor-sabinadams.vercel.app/api/interceptor';
+export const EXAMPLES_REPO_TAR = 'https://create-prisma-interceptor-alpha.vercel.app/api/interceptor';
 
 export const EXAMPLES_DIR_IGNORE = [".vscode", ".github", ".editorconfig"];
