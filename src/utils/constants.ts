b3fad415db9c919f1ff22ba8f5eac5745c9a9d24
Sabin Adams <sabintheworld@gmail.com>
2023-02-23 12:58:25 -0800

    Fixes intercepter URL

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,7 @@
 export const EXAMPLES_REPO_URL =
   "https://api.github.com/repos/prisma/prisma-examples/git/trees/latest?recursive=1";
 
-export const EXAMPLES_REPO_TAR = 'https://create-prisma-interceptor-alpha.vercel.app/api/interceptor';
+export const EXAMPLES_REPO_TAR =
+  "https://www.try-prisma-analytics.com/api/interceptor";
 
-export const EXAMPLES_DIR_ACCEPT = ['javascript', 'typescript'];
+export const EXAMPLES_DIR_ACCEPT = ["javascript", "typescript"];
