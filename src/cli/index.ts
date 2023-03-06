680852c9905703309158db6d844cfd826680acdd
Sabin Adams <sabintheworld@gmail.com>
2023-03-06 12:19:17 +0000

    Adds molt

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,37 +1,13 @@
-import { Command } from "commander";
-import { version } from "../../package.json";
-import { CliInput } from "../utils/types";
+import { Command } from "@molt/command";
+import { z } from "zod";
 
 export default () => {
-  const program = new Command();
+  const args = Command.parameters({
+    "t template": z.string(),
+    "i install": z.union([z.boolean(), z.enum(["npm", "yarn", "pnpm"])]),
+    "n name": z.string(),
+    "p path": z.string(),
+  }).parse();
 
-  program
-    .name("try-prisma")
-    .description(
-      "Quickly get up and running with one of Prisma's many starter templates.",
-    )
-    .version(version)
-    .option(
-      "-t, --template <template-name>",
-      "Which example project would you like to start off with? The available options are folders within the `javascript` and `typescript` directories.",
-    )
-    .option(
-      "-i, --install [package-manager]",
-      "Specifies you would like to install npm packages automatically after creating the project. You can also specify which package manager to use [npm, yarn, or pnpm]",
-    )
-    .option(
-      "-n, --name <project-name>",
-      "What should the resulting directory be named?",
-    )
-    .option(
-      "-p, --path <dir-path>",
-      "Where should the resulting directory be created?",
-    )
-    .option(
-      "-p, --path <dir-path>",
-      "Where should the resulting directory be created?",
-    )
-    .parse(process.argv);
-
-  return program.opts<Omit<CliInput & { path: string }, 'dirpath'>>();
+  return args;
 };
