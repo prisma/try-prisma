ab1aa9ee3a88c51709613a0ab505d189ccee0e7f
Sabin Adams <sabintheworld@gmail.com>
2023-03-07 17:06:47 +0000

    Switches over from Commander to Molt by Jason Kuhrt

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -3,10 +3,21 @@ import { z } from "zod";
 
 export default () => {
   const args = Command.parameters({
-    "t template": z.string(),
-    "i install": z.union([z.boolean(), z.enum(["npm", "yarn", "pnpm"])]),
-    "n name": z.string(),
-    "p path": z.string(),
+    "t template": z
+      .string()
+      .optional()
+      .describe(
+        "Available options are folders within the `javascript` and `typescript` directories.",
+      ),
+    "i install": z
+      .union([
+        z.boolean().describe("Using -i or --install will assume `true`"),
+        z.enum(["npm", "yarn", "pnpm"]).describe("Select a package manager"),
+      ])
+      .describe("Automatically install packages?")
+      .default(false),
+    "n name": z.string().optional().describe("Resulting directory's name"),
+    "p path": z.string().optional().describe("Resulting directory's location"),
   }).parse();
 
   return args;
