3f8fc259d189505192a36313157f7c52e2dc7798
Ankur Datta <datta@prisma.io>
2024-06-21 15:24:33 +0600

    fix: add link

--- src/index.ts
+++ src/index.ts
@@ -73,9 +73,10 @@ const main = async () => {
   );
 
   logger.success(`
-    ${chalk.bold(`The project is good to go!`)}
-    ${"Please follow the instructions in the project README to run it."}
-    `);
+    ${chalk.bold(`The project is good to go! Next steps:`)}
+    ${"Please follow the instructions in the project README to run it:"}
+    ${chalk.bold(`https://github.com/prisma/prisma-examples/tree/latest/${input.template}`)}
+    `)
 
   // } else {
   //   logger.success(`
